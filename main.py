39997e8a6714e22d7a30880f595a640647d8e0d1
qvyvu <quynhvyv@uci.edu>
2026-04-12 23:32:14 -0700

    Added find_non_injective_pair

--- main.py
+++ main.py
@@ -5,7 +5,14 @@ def find_non_injective_pair(mapping: dict) -> tuple | None:
     """Return (x1, x2) where f(x1)==f(x2) and x1!=x2, or None if injective."""
     # === TODO ===
     # Your code here
-    pass
+    if len(set(mapping.values())) == len(mapping):
+        return None
+    pairs = ()
+    for first_x, first_y in mapping.items():
+        for second_x, second_y in mapping.items():
+            if first_y == second_y and first_x != second_x:
+                pairs += ((first_x, second_x))
+    return pairs if pairs else None
     # === END TODO ===
 
 
